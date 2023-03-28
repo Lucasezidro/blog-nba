336b17320fe17d45621d2acf8cc393022ad8f3f4
lucasezidro <lucasezidro7@gmail.com>
2023-03-28 19:06:32 -0300

    config eslint and prettier

--- src/modules/components/Button/button.tsx
+++ src/modules/components/Button/button.tsx
@@ -1,41 +1,38 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 interface StyledButtonProps {
-  color?: "red" | "blue";
-  size?: "small" | "large"
+  color?: 'red' | 'blue';
+  size?: 'small' | 'large';
 }
 
 export const Button = styled.button<StyledButtonProps>`
-    width: 200px;
-    padding: ${(props) => props.size === "small" ? 0.4 : 1}rem;
-    border-radius: 1rem;
-    border: none;
-    background: ${(props) => props.color === "blue" ? "#457b9d" : "#e63946"};
-    color: #fafafa;
-    cursor: pointer;
-    font-weight: bold;
-    font-size: 15px;
-    transition: all 0.6s;
+  width: 200px;
+  padding: ${(props) => (props.size === 'small' ? 0.4 : 1)}rem;
+  border-radius: 1rem;
+  border: none;
+  background: ${(props) => (props.color === 'blue' ? '#457b9d' : '#e63946')};
+  color: #fafafa;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 15px;
+  transition: all 0.6s;
 
-    &:hover {
-      filter: brightness(0.8)
-    }
+  &:hover {
+    filter: brightness(0.8);
+  }
 `
 
 type ButtonProps = StyledButtonProps & {
   text: string;
-  onClick?: () => void
-}
+  onClick?: () => void;
+};
 
-export function Buttons ({ text, color, size, onClick }: ButtonProps) {
-  return (
-      <Button 
-        size={size} 
-        color={color}
-        onClick={onClick}
-      >
-        {text}
-      </Button>
-  )
+export function Buttons({ text, color, size, onClick }: ButtonProps) {
+
+	return (
+		<Button size={size} color={color} onClick={onClick}>
+			{text}
+		</Button>
+	)
 }
